da5ebf4a743b2cf56cceb6dbab3d1d176ed492af
Anas Tawfeek <anas.tawfeek@gmail.com>
2018-08-20 23:38:10 +0200

    feat: Implement react-router v4 (issues#1)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import logo from './logo.png'
+import './App.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="A.T.U Logo" />
+          <h1 className="App-title">A.T.U Book</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-container">
+            <Switch>
+                <Route exact path="/" render={props => <div>Posts</div>} />
+                <Route exact path="/post/:id" render={props => <div>Single post: {props.match.params.id}</div>}/>
+                <Redirect exact from='*' to='/' />
+            </Switch>
+        </div>
       </div>
     );
   }
